8b721b8a3f86aa550ee20d9f68dcd0d8c21fd4a5
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-10-18 10:28:55 +0700

    feat: move the code around and make a bird isolated

--- scene.go
+++ scene.go
@@ -10,8 +10,8 @@ import (
 )
 
 type scene struct {
-	time  int
 	bg    *sdl.Texture
+	bird  *bird
 	birds []*sdl.Texture
 }
 
@@ -21,17 +21,12 @@ func newScene(r *sdl.Renderer) (*scene, error) {
 		return nil, fmt.Errorf("could not load background image: %v", err)
 	}
 
-	var birds []*sdl.Texture
-	for i := 1; i <= 4; i++ {
-		path := fmt.Sprintf("res/imgs/frame-%d.png", i)
-		bird, err := img.LoadTexture(r, path)
-		if err != nil {
-			return nil, fmt.Errorf("could not load background image: %v", err)
-		}
-		birds = append(birds, bird)
+	bird, err := newBird(r)
+	if err != nil {
+		return nil, err
 	}
 
-	return &scene{bg: bg, birds: birds}, nil
+	return &scene{bg: bg, bird: bird}, nil
 }
 
 func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
@@ -39,7 +34,7 @@ func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
 
 	go func() {
 		defer close(errc)
-		for range time.Tick(100 * time.Millisecond) {
+		for range time.Tick(10 * time.Millisecond) {
 			select {
 			case <-ctx.Done():
 				return
@@ -55,19 +50,14 @@ func (s *scene) run(ctx context.Context, r *sdl.Renderer) <-chan error {
 }
 
 func (s *scene) paint(r *sdl.Renderer) error {
-	s.time++
-
 	r.Clear()
 
 	if err := r.Copy(s.bg, nil, nil); err != nil {
 		return fmt.Errorf("could not copy background: %v", err)
 	}
 
-	rect := &sdl.Rect{X: 10, Y: 300 - 43/2, W: 50, H: 43}
-
-	i := s.time / 10 % len(s.birds)
-	if err := r.Copy(s.birds[i], nil, rect); err != nil {
-		return fmt.Errorf("could not copy bird: %v", err)
+	if err := s.bird.paint(r); err != nil {
+		return err
 	}
 
 	defer r.Present()
@@ -76,4 +66,5 @@ func (s *scene) paint(r *sdl.Renderer) error {
 
 func (s *scene) destroy() {
 	s.bg.Destroy()
+	s.bird.destroy()
 }
